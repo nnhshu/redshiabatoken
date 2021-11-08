e4da3233019d99adac2e2d2e55441a8c4b6c0bf2
Nhat Hao Nguyen <nnhshu97@gmail.com>
2021-11-08 16:29:18 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/*=================================================================
+[Table of Content]
+
+01: Hero slider
+02: Menu Page Menu Sticky
+03: Category Section
+04: Feedback Section 
+05: live Auctions Section
+06: Client Section
+07: Collection slider 
+08: Gallery and Slider animated
+09: Preloader
+10: Document Ready 
+11: Window Load
+12: Window Scroll
+ 
+====================================================================*/
+ 
+ (function($) {
+    'use strict';
+    /*-------------------------------------
+       02: Menu Page Menu Sticky
+    -------------------------------------*/ 
+
+    function stickyHeader() {      
+        var scroll = $(window).scrollTop();
+        if (scroll > 70)
+          $('.menu_wrapper').addClass('menu_sticky');
+        else
+          $('.menu_wrapper').removeClass('menu_sticky');
+    }
+
+    /*--------------------
+     12: Window Scroll
+    ----------------------*/
+    $(window).on("scroll", function() {    
+        stickyHeader();
+     });
+ 
+})(jQuery);
